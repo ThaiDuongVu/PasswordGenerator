7cfbaf0a21b59014786c41772adf10d21bb413d6
Thai Duong Vu <vu.t.duong01@gmail.com>
2019-09-24 10:47:33 -0400

    Widgets references

--- app/src/main/java/com/thaiduong/passwordgenerator/MainActivity.java
+++ app/src/main/java/com/thaiduong/passwordgenerator/MainActivity.java
@@ -3,21 +3,55 @@ package com.thaiduong.passwordgenerator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText numberInput;
+    private EditText lengthInput;
 
     private CheckBox lowercaseCheckBox;
     private CheckBox uppercaseCheckBox;
     private CheckBox numberCheckBox;
     private CheckBox specialCharacterCheckBox;
 
+    private TextView passwordTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lengthInput = findViewById(R.id.lengthInput);
+
+        lowercaseCheckBox = findViewById(R.id.lowercaseCheckBox);
+        uppercaseCheckBox = findViewById(R.id.uppercaseCheckBox);
+        numberCheckBox = findViewById(R.id.numberCheckBox);
+        specialCharacterCheckBox = findViewById(R.id.specialCharacterCheckBox);
+
+        passwordTextView = findViewById(R.id.password);
+    }
+
+    public void onGenerateButtonClicked(View view)
+    {
+        passwordTextView.setText(GeneratePassword());
+    }
+
+    private String GeneratePassword()
+    {
+        String password = "";
+        String passwordBase = "";
+
+        int passwordLength = Integer.parseInt(lengthInput.getText().toString());
+
+        String lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+        String uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        String numbers = "1234567890";
+        String specialCharacters = "~!@#$%^&*()[]{};:<>,.?/|+-";
+
+        return password;
     }
 }
