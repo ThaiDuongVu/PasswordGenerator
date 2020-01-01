d87465af7ddec012a32a4970899c7f245edecbae
Thai Duong Vu <vu.t.duong01@gmail.com>
2020-01-01 10:15:07 -0500

    Reformat code

--- app/src/main/java/com/thaiduong/passwordgenerator/MainActivity.java
+++ app/src/main/java/com/thaiduong/passwordgenerator/MainActivity.java
@@ -1,7 +1,5 @@
 package com.thaiduong.passwordgenerator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
@@ -13,21 +11,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    Random random = new Random();
     private EditText lengthInput;
-
     private CheckBox lowercaseCheckBox;
     private CheckBox uppercaseCheckBox;
     private CheckBox numberCheckBox;
     private CheckBox specialCharacterCheckBox;
-
     private TextView passwordTextView;
-
-    Random random = new Random();
-
     private Boolean passwordGenerated;
     private String password = "";
 
@@ -48,17 +44,12 @@ public class MainActivity extends AppCompatActivity {
         passwordGenerated = false;
     }
 
-    public void onGenerateButtonClicked(View view)
-    {
+    public void onGenerateButtonClicked(View view) {
         passwordTextView.setText(GeneratePassword());
     }
 
-    private String GeneratePassword()
-    {
+    private String GeneratePassword() {
         String passwordBase = "";
-
-        StringBuilder passwordStringBuilder = new StringBuilder(password);
-        StringBuilder passwordBaseStringBuilder = new StringBuilder(passwordBase);
 
         int passwordLength = getNumber(lengthInput);
 
@@ -68,45 +59,35 @@ public class MainActivity extends AppCompatActivity {
         String numbers = "1234567890";
         String specialCharacters = "~!@#$%^&*()[]{};:<>,.?/|+-";
 
-        if (passwordLength < 0)
-        {
-            password = "Password length cannot be negative!";
+        if (passwordLength < 0) {
+            Toast.makeText(this, "Password length cannot be negative!", Toast.LENGTH_SHORT).show();
         }
-        if (passwordLength == 0)
-        {
-            password = "Password length cannot be zero!";
+        if (passwordLength == 0) {
+            Toast.makeText(this, "Password length cannot be zero!", Toast.LENGTH_SHORT).show();
         }
-        if (passwordLength > 30)
-        {
-            password = "Password cannot be longer that 30 characters!";
+        if (passwordLength > 30) {
+            Toast.makeText(this, "Password cannot be longer than 30 characters!", Toast.LENGTH_SHORT).show();
         }
 
-        if (!lowercaseCheckBox.isChecked() && !uppercaseCheckBox.isChecked() && !numberCheckBox.isChecked() && !specialCharacterCheckBox.isChecked())
-        {
-            password = "Please check at least one box above!";
+        if (!lowercaseCheckBox.isChecked() && !uppercaseCheckBox.isChecked() && !numberCheckBox.isChecked() && !specialCharacterCheckBox.isChecked()) {
+            Toast.makeText(this, "Please check at least one box above!", Toast.LENGTH_SHORT).show();
         }
-        if (lowercaseCheckBox.isChecked() || uppercaseCheckBox.isChecked() || numberCheckBox.isChecked() || specialCharacterCheckBox.isChecked())
-        {
+        if (lowercaseCheckBox.isChecked() || uppercaseCheckBox.isChecked() || numberCheckBox.isChecked() || specialCharacterCheckBox.isChecked()) {
             password = "";
-            if (lowercaseCheckBox.isChecked())
-            {
+            if (lowercaseCheckBox.isChecked()) {
                 passwordBase += lowercaseLetters;
             }
-            if (uppercaseCheckBox.isChecked())
-            {
+            if (uppercaseCheckBox.isChecked()) {
                 passwordBase += uppercaseLetters;
             }
-            if (numberCheckBox.isChecked())
-            {
+            if (numberCheckBox.isChecked()) {
                 passwordBase += numbers;
             }
-            if (specialCharacterCheckBox.isChecked())
-            {
+            if (specialCharacterCheckBox.isChecked()) {
                 passwordBase += specialCharacters;
             }
 
-            for (int i = 0; i < passwordLength; i++)
-            {
+            for (int i = 0; i < passwordLength; i++) {
                 password += passwordBase.charAt(random.nextInt(passwordBase.length()));
             }
             passwordGenerated = true;
@@ -115,31 +96,24 @@ public class MainActivity extends AppCompatActivity {
         return password;
     }
 
-    private int getNumber(EditText editText)
-    {
-        if (TextUtils.isEmpty(editText.getText()))
-        {
+    private int getNumber(EditText editText) {
+        if (TextUtils.isEmpty(editText.getText())) {
             return 0;
-        }
-            else
-        {
+        } else {
             return Integer.parseInt(editText.getText().toString());
         }
     }
 
-    public void onCopyButtonPressed(View view)
-    {
-        if (!passwordGenerated)
-        {
-            Toast.makeText(getApplicationContext(), "Password not generated yet!", Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
+    public void onCopyButtonPressed(View view) {
+        if (!passwordGenerated) {
+            Toast.makeText(this, "Password not generated yet!", Toast.LENGTH_SHORT).show();
+        } else {
             ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
             ClipData clipData = ClipData.newPlainText(password, password);
 
+            assert clipboardManager != null;
             clipboardManager.setPrimaryClip(clipData);
-            Toast.makeText(getApplicationContext(), "Password copied", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Password copied", Toast.LENGTH_SHORT).show();
         }
     }
 }
